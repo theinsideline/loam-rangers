f5ba2db5e629a6cfb79ae313093d29de553227c2
Volodymyr Lysenko <morgota199@gmail.com>
2024-04-02 21:32:34 +0300

    Update Install Script

--- load-script.js
+++ load-script.js
@@ -1,28 +1,29 @@
 //LOAD "THE LOAM RANGERS" PAGE FROM GIT REPOSITORY
-const createScriptElement = (url) => {
-  const scriptElement = document.createElement("script");
-  scriptElement.defer = true;
-  scriptElement.src = url;
+const LoamRangers = {
+    createScriptElement: url => {
+        const scriptElement = document.createElement('script')
+        scriptElement.defer = true
+        scriptElement.src = url
 
-  return scriptElement;
-};
+        return scriptElement
+    },
 
-const createStyleElement = (url) => {
-  const styleElement = document.createElement("link");
-  styleElement.rel = "stylesheet";
-  styleElement.href = url;
+    createStyleElement: url => {
+        const styleElement = document.createElement('link')
+        styleElement.rel = 'stylesheet'
+        styleElement.href = url
 
-  return styleElement;
-};
+        return styleElement
+    },
+    load: () => {
+        const appElement = createScriptElement('https://theinsideline.github.io/loam-rangers/js/index.js')
+        const chunkElement = createScriptElement('https://theinsideline.github.io/loam-rangers/js/chunk-vendors.js')
+        const styleElement = createStyleElement('https://theinsideline.github.io/loam-rangers/css/index.css')
 
-const load = () => {
-  const appElement = createScriptElement("https://theinsideline.github.io/loam-rangers/js/index.js");
-  const chunkElement = createScriptElement("https://theinsideline.github.io/loam-rangers/js/chunk-vendors.js");
-  const styleElement = createStyleElement("https://theinsideline.github.io/loam-rangers/css/index.css");
+        document.head.append(appElement)
+        document.head.append(chunkElement)
+        document.head.append(styleElement)
+    },
+}
 
-  document.head.append(appElement);
-  document.head.append(chunkElement);
-  document.head.append(styleElement);
-};
-
-document.addEventListener("DOMContentLoaded", load);
+document.addEventListener('DOMContentLoaded', LoamRangers.load)
