6801214ff0e123254a184d049409d71292ca7a4a
Volodymyr Lysenko <morgota199@gmail.com>
2024-04-02 22:06:52 +0300

    Update Install Script

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,24 @@
+<script setup lang="ts">
+    import { onMounted } from 'vue'
+
+    onMounted(() => {
+        const headerElement = document.querySelector('h1.margin-bottom-sm')
+        if (!headerElement) return
+
+        const header = (headerElement.textContent || '').toLowerCase()
+        if (!header.length) return
+
+        if (!header.includes('loam rangers')) return
+
+        headerElement.remove()
+
+        const section = document.querySelector('section.section-control.container')
+        if (!section) return
+
+        section.classList.add('padding-top-null')
+    })
+</script>
+
 <template>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
@@ -16,12 +37,12 @@
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
+    .padding-top-null {
+        padding-top: 0 !important;
+    }
+
+    #loam_rangers {
+        margin-left: auto;
+        margin-right: auto;
     }
 </style>
