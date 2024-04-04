70366592e155293bccc8f95f912baa8411c2a2e7
Volodymyr Lysenko <morgota199@gmail.com>
2024-04-05 02:45:19 +0300

    add page description

--- src/components/page-title.vue
+++ src/components/page-title.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-    import { Heading1 } from '@theinsideline/common'
+    import { Heading1, Paragraph } from '@theinsideline/common'
 </script>
 
 <template>
     <Heading1 class="page-title" text="The Loam Ranger" />
+    <Paragraph class="page-description">
+        <strong>Ride What Loam Rides — </strong>The Loam Ranger’s go to Bikes, Components, Accessories, and Apparel
+    </Paragraph>
 </template>
 
 <style scoped lang="scss">
@@ -30,6 +33,28 @@
 
         @include tv {
             margin-top: 60px;
+        }
+    }
+
+    .page-description {
+        @include mobile {
+            margin-top: 15px;
+        }
+
+        @include tablet {
+            margin-top: 20px;
+        }
+
+        @include laptop {
+            margin-top: 20px;
+        }
+
+        @include desktop {
+            margin-top: 20px;
+        }
+
+        @include tv {
+            margin-top: 30px;
         }
     }
 </style>
