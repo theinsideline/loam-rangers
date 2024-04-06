7a2d2adb8ebfc0e4d1bc05c4d7279cbf58f603f9
Volodymyr Lysenko <morgota199@gmail.com>
2024-04-06 07:56:09 +0300

    Create loam deal

--- src/components/the-latest-video.vue
+++ src/components/the-latest-video.vue
@@ -1,29 +1,103 @@
 <script setup lang="ts">
-    import { Heading2 } from '@theinsideline/common'
+    import { LATEST_VIDEO } from '@/constants'
+    import { Heading2, LinkButton, Paragraph } from '@theinsideline/common'
 </script>
 
 <template>
     <section class="latest-video">
         <Heading2 text="Latest Video" />
+
+        <div class="lates-video__content">
+            <div class="content content__title" :style="`background: ${LATEST_VIDEO.BACKGROUND};`">
+                <Heading2 :text="LATEST_VIDEO.TITLE" />
+                <Paragraph :text="LATEST_VIDEO.DESCRIPTION" />
+                <LinkButton color="white" :link="LATEST_VIDEO.VIDEO_LINK" text="Watch now" />
+            </div>
+            <div class="content content__preview" :style="`background-image: url(${LATEST_VIDEO.IMAGE_LINK});`"></div>
+        </div>
     </section>
 </template>
 
 <style scoped lang="scss">
     .latest-video {
+        &__content {
+            display: flex;
+        }
+
+        .content {
+            width: 50%;
+        }
+
+        .content__preview {
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
         @include mobile {
             margin-top: 35px;
+
+            &__content {
+                flex-direction: column-reverse;
+            }
+
+            .content {
+                width: 100%;
+                height: 340px;
+            }
         }
 
         @include tablet {
             margin-top: 45px;
+
+            &__content {
+                flex-direction: column-reverse;
+            }
+
+            .content {
+                width: 100%;
+                height: 340px;
+            }
         }
 
         @include laptop {
             margin-top: 45px;
+
+            &__content {
+                flex-direction: column-reverse;
+            }
+
+            .content {
+                width: 100%;
+            }
+
+            .content__preview {
+                height: 500px;
+            }
+
+            .content__title {
+                height: 350px;
+            }
         }
 
         @include desktop {
             margin-top: 45px;
+
+            &__content {
+                flex-direction: column-reverse;
+            }
+
+            .content {
+                width: 100%;
+            }
+
+            .content__preview {
+                height: 500px;
+            }
+
+            .content__title {
+                height: 350px;
+            }
         }
 
         @include tv {
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,9 @@
 export const PUBLIC_DOMAIN = 'https://theinsideline.github.io/loam-rangers'
+
+export const LATEST_VIDEO = {
+    TITLE: 'The 40 Foot Death Gap',
+    DESCRIPTION: 'The Loam Ranger heads to our local jump trail, Flight 66 on Moose Mountain to prove that the Ripmo can truly do it all',
+    BACKGROUND: '#192026',
+    VIDEO_LINK: 'https://www.youtube.com/embed/ocp5ynefpDk',
+    IMAGE_LINK: 'https://theinsideline.github.io/loam-rangers/images/Background.png',
+}
