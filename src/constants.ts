cdcf087f05d3b60251d675e6c18f29a8cb1c6539
yuriyramone <83171091+yuriyramone@users.noreply.github.com>
2024-04-18 15:22:45 -0600

    constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 export const PUBLIC_DOMAIN = 'https://theinsideline.github.io/loam-rangers'
 
 export const LATEST_VIDEO = {
-    TITLE: 'The 40 Foot Death Gap',
-    DESCRIPTION: 'The Loam Ranger heads to our local jump trail, Flight 66 on Moose Mountain to prove that the Ripmo can truly do it all!',
-    BACKGROUND: '#192026',
-    VIDEO_LINK: 'https://www.youtube.com/embed/ocp5ynefpDk',
-    IMAGE_LINK: 'https://theinsideline.github.io/loam-rangers/images/Background.png',
+    TITLE: 'Prince George Trails',
+    DESCRIPTION: 'A trip to the Emergency Room was NOT in the plans',
+    BACKGROUND: '#18130D',
+    VIDEO_LINK: 'https://youtu.be/8OphRD__W7o?si=zbSq54BMyCLOh7zg',
+    IMAGE_LINK: 'https://cdn.shoplightspeed.com/shops/638615/files/62893761/the-loam-ranger-pgt.jpg',
 }
