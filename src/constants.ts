57563bdead791e0408a324f25664ebe3f502ca90
yuriyramone <83171091+yuriyramone@users.noreply.github.com>
2024-07-24 11:10:02 -0600

    constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 export const PUBLIC_DOMAIN = 'https://theinsideline.github.io/loam-rangers'
 
 export const LATEST_VIDEO = {
-    TITLE: 'The New Ibis RIPMO and RIPLEY',
+    TITLE: 'The New Ibis Ripmo & Ripley',
     DESCRIPTION: 'They are here. And they are very, very good!',
     BACKGROUND: '#05100B',
-    VIDEO_LINK: 'https://youtu.be/Khoq8E_Y3lM?si=oNilVdDnnklsjwC6',
+    VIDEO_LINK: 'https://youtu.be/Khoq8E_Y3lM',
     IMAGE_LINK: 'https://cdn.shoplightspeed.com/shops/638615/files/64789583/ibis-ripmo-3-olive-green-swamp-monster-2.jpg',
 }
