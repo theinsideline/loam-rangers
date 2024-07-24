38adc98c497621b513dae835396e30a7a95431bf
yuriyramone <83171091+yuriyramone@users.noreply.github.com>
2024-07-24 11:04:57 -0600

    constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 export const PUBLIC_DOMAIN = 'https://theinsideline.github.io/loam-rangers'
 
 export const LATEST_VIDEO = {
-    TITLE: 'Prince George Trails',
-    DESCRIPTION: 'A trip to the Emergency Room was NOT in the plans',
-    BACKGROUND: '#18130D',
-    VIDEO_LINK: 'https://youtu.be/8OphRD__W7o?si=zbSq54BMyCLOh7zg',
-    IMAGE_LINK: 'https://cdn.shoplightspeed.com/shops/638615/files/62893761/the-loam-ranger-pgt.jpg',
+    TITLE: 'The New Ibis RIPMO and RIPLEY',
+    DESCRIPTION: 'They are here. And they are very, very good!',
+    BACKGROUND: '#05100B',
+    VIDEO_LINK: 'https://youtu.be/Khoq8E_Y3lM?si=oNilVdDnnklsjwC6',
+    IMAGE_LINK: 'https://cdn.shoplightspeed.com/shops/638615/files/64789583/ibis-ripmo-3-olive-green-swamp-monster-2.jpg',
 }
